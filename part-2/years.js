5ff24e07894a265073a44cae6343545e257f7cea
bj bautista <emailme@djbrigidope.com>
2017-08-15 21:59:10 -0700

    syntax errors

--- part-2/years.js
+++ part-2/years.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 const cars = fs.readFileSync('./cars.json'.toString());
 const carsObj = JSON.parse(cars);
-console.log(carsObj);
+// console.log(carsObj);
 const years = process.argv[2];
 
 
 const yearInRange = years => {
   let range = years.split('-');
-
-
-  console.log(range);
+  let min = range[0]*1;
+  let max = range[1]*1;
+  console.log(min);
+  console.log(max);
+  return carsObj.filter(car => min <= car.year && max >= car.year)
+  // console.log(range);
 }
 
-yearInRange(years);
+console.log(yearInRange(years));
 // }))
 // console.log(modelResults)
 // };
