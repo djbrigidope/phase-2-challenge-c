686edb4fff2036ccf634981ac117711483b4c423
bj bautista <emailme@djbrigidope.com>
2017-08-31 15:23:28 -0700

    ayyy merge sort this biih

--- part-1/functions.js
+++ part-1/functions.js
@@ -39,5 +39,29 @@ console.log(cals(new Date(2017, 08, 19)));
 
 // capitolize every 4th letter
 
+loop through array, modulous index by 4. then toUpperCase dat bih
 
+function capitalizeFourth () {
+  
+}
+
+
+
+
+
+
+capitalizeFourth("Eenie, Meenie, Miney, Moe") // => "eenIe, MeenIe, MineY, mOe"
+capitalizeFourth("ABRACADABRA") // => "abrAcadAbra"
 //whew
+
+
+
+//write a function `getValues(obj)` that returns
+// all the values for an object. ignore symbolic properties & count only 'own properies'
+
+
+
+
+
+//write a function that takes an array of strings, a lower value,
+// and an upper value It returns a new array containing _only_ the elements from the source `array` that come before `lower` alphabetically and after `upper`. The elements in the returned array should be in the same order as the source array.
